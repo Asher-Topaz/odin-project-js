f1830898e749b35507dfc85960611d74b45c1b51
asher <asheranumah@gmail.com>
2023-03-17 12:30:18 +0100

    copied data from one variable to another

---
+++ Variables/variables-2/javascript.js
@@ -0,0 +1,12 @@
+// decalare two variables and copy data from one into the other
+
+let hello = 'Hello World';
+
+let message;
+
+//duplicate data from hello into message
+
+message = hello ;
+
+alert(hello);
+alert(message); 
