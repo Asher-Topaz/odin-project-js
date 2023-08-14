3a1553d6a2bac5127579b996904e2f47d932e74d
ashertopaz <asheranumah@gmail.com>
2023-08-14 19:17:10 +0100

    added comment about valueOf() method

--- strings_/string_prim_obj/script2.js
+++ strings_/string_prim_obj/script2.js
@@ -3,4 +3,5 @@ const s2 = new String("2+2");// creates a string object
 console.log(eval(s1));
 console.log(eval(s2));
 
-console.log(eval(s2.valueOf()));
+console.log(eval(s2.valueOf())); // method converts object to primitive
+
